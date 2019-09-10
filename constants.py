a1da027f30f9a3a9afcec605c834d69839e1e156
hsbk <hieubkset@gmail.com>
2019-09-10 17:27:53 +0900

    fix tts source api and use relatve path to save audio file

--- constants.py
+++ constants.py
@@ -1,9 +1,20 @@
-TEXT_TO_SPEECH_SRC = 'https://soundoftext.com/api/sounds'
+# To find the TEXT_TO_SPEECH_SRC
+# go to the https://soundoftext.com/
+# make a mock download
+# press F12
+# go to network tab
+# see headers --> request url
+TEXT_TO_SPEECH_SRC = 'https://api.soundoftext.com/sounds'
 SOUNDS_POST_CMD = '{{"engine": "Google", "data": {{"text": "{0}", "voice": "{1}"}}}}'
-TEMP_FILE_PATH = "/Volumes/Data/Todo/Python/MemriseRobot/audio/temp.mp3"
+#
+import os
+cwd = os.getcwd()
+RELATIVE_TEMP_FILE_PATH = "audio/temp.mp3"
+TEMP_FILE_PATH = os.path.join(cwd, RELATIVE_TEMP_FILE_PATH)
+#
 ENGLISH_SRC = 'https://www.oxfordlearnersdictionaries.com/definition/english/'
 MEMRISE_HOME = "https://www.memrise.com/home/"
 USERNAME = 'memrise.robot'
-PASSWORD = 'hieu0611@'
+PASSWORD = '123456'
 DELAY_TIME = 1
 PRONUNCIATION = False
